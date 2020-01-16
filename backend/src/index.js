9139c99a2aec6e5d787233fb93d84dea5aaf42c7
Rodrigo Teizen Prado Spinelli - CE <rodrigo.spinelli@exclusivasex.net>
2020-01-16 16:09:22 -0300

    configurando rotas, controller de dev criados, model adicionado.

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://DIGOYB:5927RTyb@cluster0-ff5tx.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+// métodos http: get, post, put, delete
+
+// tipos de parâmetros:
+// query params: request.query (filtros, paginação, ordenação, ...)
+// route params: request.params () 
+// body: request.body
+
+//mongodb (Não-relacional)
+
+
+app.listen(3333);
