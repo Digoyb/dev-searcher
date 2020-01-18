52d7e70f187b042ea6a0cd38658de9833c2d3c4c
rodrigo.spinelli <digoh_straight@hotmail.com>
2020-01-18 14:28:21 -0300

    projeto web adicionado, juntamente com aside e main e estilização.

---
+++ web/src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import "./global.css";
+import "./app.css";
+import "./sidebar.css";
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="username_github">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+
+            <div class="input-block">
+              <label htmlFor="latidude">Latitude</label>
+              <input name="latidude" id="latidude" required />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
