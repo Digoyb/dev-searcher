ed9ce69eec2a936805f2ca803ba9a0cb3b685d7a
rodrigo.spinelli <digoh_straight@hotmail.com>
2020-01-18 16:24:49 -0300

    consumo de api e arquitetura refatorada e abstraida, front end completo.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,8 @@ mongoose.connect('mongodb+srv://DIGOYB:5927RTyb@cluster0-ff5tx.mongodb.net/test?
     useUnifiedTopology: true,
 });
 
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
+
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
 
 import "./global.css";
 import "./app.css";
@@ -6,98 +10,37 @@ import "./sidebar.css";
 import "./main.css";
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
-            <label htmlFor="username_github">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div class="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-
-            <div class="input-block">
-              <label htmlFor="latidude">Latitude</label>
-              <input name="latidude" id="latidude" required />
-            </div>
-
-            <div class="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
-
       <main>
-
-      <ul>
-        <li className="dev-item">
-          <header>
-            <img src="" alt=""/>
-            <div className="user-info">
-              <strong>Rodrigo Spinelli</strong>
-              <span>React, React Native, Node.js</span>
-            </div>
-          </header>
-          <p>CTO na Rodrigo Spinelli Dev, apaixonado pelas melhores tecnologias</p>
-        </li>
-
-        <li className="dev-item">
-          <header>
-            <img src="" alt=""/>
-            <div className="user-info">
-              <strong>Rodrigo Spinelli</strong>
-              <span>React, React Native, Node.js</span>
-            </div>
-          </header>
-          <p>CTO na Rodrigo Spinelli Dev, apaixonado pelas melhores tecnologias</p>
-        </li>
-
-        <li className="dev-item">
-          <header>
-            <img src="" alt=""/>
-            <div className="user-info">
-              <strong>Rodrigo Spinelli</strong>
-              <span>React, React Native, Node.js</span>
-            </div>
-          </header>
-          <p>CTO na Rodrigo Spinelli Dev, apaixonado pelas melhores tecnologias</p>
-        </li>
-
-        <li className="dev-item">
-          <header>
-            <img src="" alt=""/>
-            <div className="user-info">
-              <strong>Rodrigo Spinelli</strong>
-              <span>React, React Native, Node.js</span>
-            </div>
-          </header>
-          <p>CTO na Rodrigo Spinelli Dev, apaixonado pelas melhores tecnologias</p>
-        </li>
-
-        <li className="dev-item">
-          <header>
-            <img src="" alt=""/>
-            <div className="user-info">
-              <strong>Rodrigo Spinelli</strong>
-              <span>React, React Native, Node.js</span>
-            </div>
-          </header>
-          <p>CTO na Rodrigo Spinelli Dev, apaixonado pelas melhores tecnologias</p>
-        </li>
-      </ul>
-
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
       </main>
     </div>
   );
